8776bae7e2dbf3b2b136b40f62ff50546f3d750f
halleyIsStressed <lee.lapyhin0127@gmail.com>
2025-04-19 15:30:16 +0800

    change @Data to @Getter @Setter
    this fucker -> @Data causing ConcurrentModificationException

--- src/main/java/com/samseng/web/models/Variant.java
+++ src/main/java/com/samseng/web/models/Variant.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.NaturalId;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "\"Variant\"")
 public class Variant {
